928acfc468c17ec2456c7579389a09a931b4f68b
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-01-21 15:16:14 -0400

    updated file

--- app/src/main/java/com/info3604/streetfoodtracker/MainActivity.java
+++ app/src/main/java/com/info3604/streetfoodtracker/MainActivity.java
@@ -49,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
