99e8b12e6d88fa5c43c1a4f77a89cf1f6953554f
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-02-07 23:57:37 -0400

    minor update

---
+++ app/src/main/java/com/info3604/streetfoodtracker/ui/user/registrationActivity.java
@@ -0,0 +1,104 @@
+package com.info3604.streetfoodtracker.ui.user;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.info3604.streetfoodtracker.R;
+
+public class registrationActivity extends AppCompatActivity {
+    private EditText name, email_id, passwordcheck;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "";
+    private ProgressBar progressBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_activity);
+
+
+        TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(singup_activity.this, singin_activity.class);
+                //startActivity(intent);
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email_id = (EditText) findViewById(R.id.input_email);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        passwordcheck = (EditText) findViewById(R.id.input_password);
+        Button ahsignup = (Button) findViewById(R.id.btn_signup);
+
+
+        ahsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = email_id.getText().toString();
+                String password = passwordcheck.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter Eamil Id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(registrationActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    //Intent intent = new Intent(singup_activity.this, Home_screen.class);
+                                    //startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(registrationActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+
+
+                        });
+            }
+        });
+
+
+    }
+}
