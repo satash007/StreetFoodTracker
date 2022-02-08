3cccbe5b838b04c827815934aa87b152e0d9e56f
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-02-07 22:48:51 -0400

    added Firebase Auth

--- app/src/main/java/com/info3604/streetfoodtracker/ui/home/HomeFragment.java
+++ app/src/main/java/com/info3604/streetfoodtracker/ui/home/HomeFragment.java
@@ -1,24 +1,37 @@
 package com.info3604.streetfoodtracker.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.info3604.streetfoodtracker.R;
 import com.info3604.streetfoodtracker.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    FragmentActivity act;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListner;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListner);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +48,29 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Button button = (Button) root.findViewById(R.id.signout);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()==null)
+                {
+                    //startActivity(new Intent(HomeFragment.this, singin_activity.class));
+                }
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+
+
+            }
+        });
+
         return root;
     }
 
