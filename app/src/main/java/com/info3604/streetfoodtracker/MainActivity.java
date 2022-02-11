45651fab6c234d2d39a6628803cc6be2ba57b158
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-02-10 22:12:28 -0400

    added application logo

--- app/src/main/java/com/info3604/streetfoodtracker/MainActivity.java
+++ app/src/main/java/com/info3604/streetfoodtracker/MainActivity.java
@@ -1,12 +1,15 @@
 package com.info3604.streetfoodtracker;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -50,18 +53,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
+    */
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
 }
--- app/src/main/java/com/info3604/streetfoodtracker/ui/user/registrationActivity.java
+++ app/src/main/java/com/info3604/streetfoodtracker/ui/user/registrationActivity.java
@@ -20,14 +20,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.info3604.streetfoodtracker.MainActivity;
 import com.info3604.streetfoodtracker.R;
 
 public class registrationActivity extends AppCompatActivity {
-    private EditText name, email_id, passwordcheck;
+    private EditText name, email_id, passwordcheck, user_type, user_address;
     private FirebaseAuth mAuth;
     private static final String TAG = "";
     private ProgressBar progressBar;
 
+    // creating a variable for our
+    // Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our Database
+    // Reference for Firebase.
+    DatabaseReference databaseReference;
+
+    // creating a variable for
+    // our object class
+    UserInfo userInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +52,13 @@ public class registrationActivity extends AppCompatActivity {
         setContentView(R.layout.registration_activity);
 
 
-        TextView btnSignUp = (TextView) findViewById(R.id.login_page);
+        TextView txtSignUp = (TextView) findViewById(R.id.login_page);
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(singup_activity.this, singin_activity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(registrationActivity.this, signInActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -51,14 +68,18 @@ public class registrationActivity extends AppCompatActivity {
         email_id = (EditText) findViewById(R.id.input_email);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         passwordcheck = (EditText) findViewById(R.id.input_password);
-        Button ahsignup = (Button) findViewById(R.id.btn_signup);
+        Button btnSignUp = (Button) findViewById(R.id.btn_signup);
+        user_type = (EditText) findViewById(R.id.input_user_type);
+        user_address = (EditText) findViewById(R.id.input_user_address);
 
 
-        ahsignup.setOnClickListener(new View.OnClickListener() {
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = email_id.getText().toString();
                 String password = passwordcheck.getText().toString();
+                String userType = user_type.getText().toString();
+                String userAddress = user_address.getText().toString();
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter Eamil Id", Toast.LENGTH_SHORT).show();
@@ -81,8 +102,9 @@ public class registrationActivity extends AppCompatActivity {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
                                     FirebaseUser user = mAuth.getCurrentUser();
-                                    //Intent intent = new Intent(singup_activity.this, Home_screen.class);
-                                    //startActivity(intent);
+                                    addDatatoFirebase(email, userAddress, userType);
+                                    Intent intent = new Intent(registrationActivity.this, MainActivity.class);
+                                    startActivity(intent);
                                     finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
@@ -96,9 +118,51 @@ public class registrationActivity extends AppCompatActivity {
 
 
                         });
+
+
             }
         });
 
+        // below line is used to get the
+        // instance of our FIrebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance();
 
+        // below line is used to get reference for our database.
+        databaseReference = firebaseDatabase.getReference("UserInfo");
+
+        // initializing our object
+        // class variable.
+        userInfo = new UserInfo();
+
+    }
+
+    private void addDatatoFirebase(String userEmail, String userAddress, String userType) {
+        // below 3 lines of code is used to set
+        // data in our object class.
+        userInfo.setUserEmail(userEmail);
+        userInfo.setUserAddress(userAddress);
+        userInfo.setUserType(userType);
+
+        // we are use add value event listener method
+        // which is called with database reference.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // inside the method of on Data change we are setting
+                // our object class to our database reference.
+                // data base reference will sends data to firebase.
+                databaseReference.setValue(userInfo);
+
+                // after adding this data we are showing toast message.
+                Toast.makeText(registrationActivity.this, "data added", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // if the data is not added or it is cancelled then
+                // we are displaying a failure toast message.
+                Toast.makeText(registrationActivity.this, "Fail to add data " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
