3386e580db1299c80814d71045f85787c2236e80
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-04-11 22:16:37 -0400

    created tabbed viewpager fragment as the home fragment added icons to viewpager tabs

---
+++ app/src/main/java/com/info3604/streetfoodtracker/HomeFragment.java
@@ -0,0 +1,156 @@
+package com.info3604.streetfoodtracker;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+public class HomeFragment extends Fragment {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private String[] tabTitles = new String[]{"Explore", "Map View", "List View"};
+
+    private int[] tabIcons = {
+            R.drawable.ic_food,
+            R.drawable.ic_map,
+            R.drawable.ic_list_vendors,
+    };
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public void selectPage(int page) {
+        viewPager.setCurrentItem(page);
+        viewPager.getAdapter().notifyDataSetChanged();
+
+    }
+
+    public static HomeFragment newInstance() {
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("HomeFragment", "onCreate");
+    }
+
+    public ViewPager getViewPager(){
+        return viewPager;
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        getActivity().setTitle("Street Food Tracker");
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) view.findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(
+                    int position,
+                    float positionOffset,
+                    int positionOffsetPixels) {
+            }
+            @Override
+            public void onPageSelected(int position) {
+                viewPager.getAdapter().notifyDataSetChanged();
+                setupTabIcons();
+
+            }
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        return view;
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFrag(new ListViewFragment(), "Explore");
+        adapter.addFrag(new VendorMapFragment(), "Map View");
+        adapter.addFrag(new VendorListFragment(), "List View");
+
+        viewPager.setAdapter(adapter);
+
+    }
+
+
+    class ViewPagerAdapter extends FragmentStatePagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public int getItemPosition(Object object) {
+        // POSITION_NONE makes it possible to reload the PagerAdapter
+            return POSITION_NONE;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFrag(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
+    }
+
+
+}
+
+
+/*
+REFERENCES
+https://stackoverflow.com/questions/18088076/update-fragment-from-viewpager
+ */
+
