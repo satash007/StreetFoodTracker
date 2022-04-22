58064bffc9fc18b6a3ad8f2423cb86e024e25398
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-04-22 11:13:56 -0400

    minor updates to test classes

--- app/src/test/java/com/info3604/streetfoodtracker/VendorModelTest.java
+++ app/src/test/java/com/info3604/streetfoodtracker/VendorModelTest.java
@@ -18,12 +18,12 @@ public class VendorModelTest {
 
     @Before
     public void intialize(){
-        vendorT = new VendorModel( "Nigel", "Penal",  "346782","-456723.00","3.0", "Firebase");
+        vendorT = new VendorModel( "Nigel", "Penal",  "346782","-456723.00","3.0", "Firebase", "56");
     }
 
 
     @Test
-    public void testGetVenName(){
+    public void testGetVendorName(){
         System.out.println("Testing getName() method in the VendorModel class...");
         String expResult = "Nigel" ;
         String actualResult = vendorT.getName();
@@ -32,24 +32,24 @@ public class VendorModelTest {
 
 
     @Test
-    public void testVenAdress(){
+    public void testGetVendorAddress(){
         System.out.println("Testing getAddress() method in the VendorModel class...");
         String expResult = "Penal" ;
-        String actualResult = vendorT.getAdress();
+        String actualResult = vendorT.getAddress();
         assertEquals(expResult, actualResult);
     }
 
 
     @Test
-    public void testVenLatitude(){
+    public void testGetVendorLatitude(){
         System.out.println("Testing  getLatitude() method in the VendorModel class...");
         String expResult = "346782";
-        String actualResult = vendorT. getLatitude();
+        String actualResult = vendorT.getLatitude();
         assertEquals(expResult, actualResult);
     }
 
     @Test
-    public void testVenLongitiude(){
+    public void testGetVendorLongitiude(){
         System.out.println("Testing getLongitiude method in the VendorModel class...");
         String expResult = "-456723.00";
         String actualResult = vendorT.getLongitude();
@@ -60,14 +60,19 @@ public class VendorModelTest {
 
 
     @Test
-    void toStringTest(){
+    public void toStringTest(){
         String toString = vendorT.toString();
         // testing the toString() method function
         // specifically the variable type that is returned respectively.
         assertTrue(toString.contains(vendorT.getName())); // name
-        assertTrue(toString.contains(vendorT.getAdress())); // address
-        // latitude, longitutde
-        // rating
+        assertTrue(toString.contains(vendorT.getAddress())); // address
+        assertTrue(toString.contains(vendorT.getLatitude())); // latitude
+        assertTrue(toString.contains(vendorT.getLongitude()));
+        assertTrue(toString.contains(vendorT.getRating()));
+        assertTrue(toString.contains(vendorT.getDistance()));
+        assertTrue(toString.contains(vendorT.getType()));
+
+        System.out.println(toString);
 
     }
 
