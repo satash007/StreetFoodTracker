b99f1e0640442ab331fcfda6a6a2e74a6a424751
DexterUWI <60389845+DexterUWI@users.noreply.github.com>
2022-04-22 11:11:59 -0400

    Wrapped the home fragment linear layouts using this custom manager class to prevent outOfIndex errors for the viewpager

---
+++ app/src/main/java/com/info3604/streetfoodtracker/utils/WrapContentLinearLayoutManager.java
@@ -0,0 +1,37 @@
+package com.info3604.streetfoodtracker.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*
+This class was written to solve a crashing issue with the ExploreFragment Tab
+The issue was caused by RecyclerView Data that has been modified in different thread.
+Checking all data access and wrapping LinearLayoutManager would be considered as a solution.
+ */
+
+public class WrapContentLinearLayoutManager extends LinearLayoutManager {
+    public WrapContentLinearLayoutManager(Context context) {
+        super(context);
+    }
+
+    public WrapContentLinearLayoutManager(Context context, int orientation, boolean reverseLayout) {
+        super(context, orientation, reverseLayout);
+    }
+
+    public WrapContentLinearLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
+        try {
+            super.onLayoutChildren(recycler, state);
+        } catch (IndexOutOfBoundsException e) {
+            Log.e("TAG", "IOOBE in RecyclerView");
+        }
+    }
+}
